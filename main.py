6afcc75dc47e61e266da2bd1a9c55b99e25d01ae
Siyanho <95035715+Siyanho@users.noreply.github.com>
2021-12-11 13:12:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,107 +21,518 @@ p1=b[t1][r1]
 t2=m-2
 r2=m-2
 p2=b[t2][r2]
-while x==1:
-    m1=input('Enter the move of player 1: ')
-    if m1!='R' and m1!='L' and m1!='U' and m1!='D':
-        print('Please enter R,L,U or D !')
-    if m1=='R':
-        b[t1][r1]=4
-        p1=b[t1][r1+1]
-        if p1!=8 and p1!=2 and p1!=4:
-            b[t1][r1+1]=1
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player2 is the winner!')
-            break
-        r1+=1
-    if m1=='L':
-        b[t1][r1]=4
-        p1=b[t1][r1-1]
-        if p1!=8 and p1!=2 and p1!=4:
-            b[t1][r1-1]=1
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player2 is the winner!')
-            break
-        r1-=1
-    if m1=='U':
-        b[t1][r1]=4
-        p1=b[t1-1][r1]
-        if p1!=8 and p1!=2 and p1!=4:
-            b[t1-1][r1]=1
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player2 is the winner!')
-            break
-        t1-=1
-    if m1=='D':
-        b[t1][r1]=4
-        p1=b[t1+1][r1]
-        if p1!=8 and p1!=2 and p1!=4:
-            b[t1+1][r1]=1
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player2 is the winner!')
-            break
-        t1+=1
 
-    m2=input('Enter the move of player 2: ')
-    if m2!='R' and m2!='L' and m2!='U' and m2!='D':
-        print('Please enter R,L,U or D !')
-    if m2=='R':
-        b[t2][r2]=4
-        p2=b[t2][r2+1]
-        if p2!=8 and p2!=1 and p2!=4:
-            b[t2][r2+1]=2
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player1 is the winner!')
-            break
-        r2+=1
-    if m2=='L':
-        b[t2][r2]=4
-        p2=b[t2][r2-1]
-        if p2!=8 and p2!=1 and p2!=4:
-            b[t2][r2-1]=2
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player1 is the winner!')
-            break
-        r2-=1
-    if m2=='U':
-        b[t2][r2]=4
-        p2=b[t2-1][r2]
-        if p2!=8 and p2!=1 and p2!=4:
-            b[t2-1][r2]=2
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player1 is the winner!')
-            break
-        t2-=1
-    if m2=='D':
-        b[t2][r2]=4
-        p2=b[t2+1][r2]
-        if p2!=8 and p2!=1 and p2!=4:
-            b[t2+1][r2]=2
-            for v in range(m):
-                print(b[v])
-        else:
-            x=0
-            print('The game is over!Player1 is the winner!')
-            break
-        t2+=1
+#choose if wanna move simultaneously
+print('                               ')
+print('===============================')
+print('Do you wanna move simultaneously?')
+anwser1=input('please enter Y or N:')
+#Move asynchronously
+if anwser1=='N':
+    while x==1:
+        m1=input('Enter the move of player 1: ')
+        if m1!='R' and m1!='L' and m1!='U' and m1!='D':
+            print('Please enter R,L,U or D !')
+        if m1=='R':
+            b[t1][r1]=4
+            p1=b[t1][r1+1]
+            if p1!=8 and p1!=2 and p1!=4:
+                b[t1][r1+1]=1
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            r1+=1
+        if m1=='L':
+            b[t1][r1]=4
+            p1=b[t1][r1-1]
+            if p1!=8 and p1!=2 and p1!=4:
+                b[t1][r1-1]=1
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            r1-=1
+        if m1=='U':
+            b[t1][r1]=4
+            p1=b[t1-1][r1]
+            if p1!=8 and p1!=2 and p1!=4:
+                b[t1-1][r1]=1
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            t1-=1
+        if m1=='D':
+            b[t1][r1]=4
+            p1=b[t1+1][r1]
+            if p1!=8 and p1!=2 and p1!=4:
+                b[t1+1][r1]=1
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            t1+=1
+    
+        m2=input('Enter the move of player 2: ')
+        if m2!='R' and m2!='L' and m2!='U' and m2!='D':
+            print('Please enter R,L,U or D !')
+        if m2=='R':
+            b[t2][r2]=4
+            p2=b[t2][r2+1]
+            if p2!=8 and p2!=1 and p2!=4:
+                b[t2][r2+1]=2
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            r2+=1
+        if m2=='L':
+            b[t2][r2]=4
+            p2=b[t2][r2-1]
+            if p2!=8 and p2!=1 and p2!=4:
+                b[t2][r2-1]=2
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            r2-=1
+        if m2=='U':
+            b[t2][r2]=4
+            p2=b[t2-1][r2]
+            if p2!=8 and p2!=1 and p2!=4:
+                b[t2-1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            t2-=1
+        if m2=='D':
+            b[t2][r2]=4
+            p2=b[t2+1][r2]
+            if p2!=8 and p2!=1 and p2!=4:
+                b[t2+1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            else:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            t2+=1
+            
+#move simultaneously
+if anwser1=='Y':
+    while x==1:
+         m1=input('Enter the move of player 1: ')
+         m2=input('Enter the move of player 2: ')
+         if (m1!='R' and m1!='L' and m1!='U' and m1!='D') or (m2!='R' and m2!='L' and m2!='U' and m2!='D'):
+            print('Please enter R,L,U or D !')
+#1
+         if m1=='R' and m2=='R':
+            b[t1][r1]=4
+            p1=b[t1][r1+1]
+            b[t2][r2]=4
+            p2=b[t2][r2+1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1][r1+1]=1
+                b[t2][r2+1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==t2 and (r1+1)==(r2+1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1+=1
+            r2+=1
+         if m1=='R' and m2=='L':
+            b[t1][r1]=4
+            p1=b[t1][r1+1]
+            b[t2][r2]=4
+            p2=b[t2][r2-1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+               b[t1][r1+1]=1
+               b[t2][r2-1]=2
+               for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==t2 and (r1+1)==(r2-1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1+=1
+            r2-=1
+         if m1=='R' and m2=='U':
+            b[t1][r1]=4
+            p1=b[t1][r1+1]
+            b[t2][r2]=4
+            p2=b[t2-1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+               b[t1][r1+1]=1
+               b[t2-1][r2]=2
+               for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==(t2-1) and (r1+1)==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1+=1
+            t2-=1
+         if m1=='R' and m2=='D':
+            b[t1][r1]=4
+            p1=b[t1][r1+1]
+            b[t2][r2]=4
+            p2=b[t2+1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+               b[t1][r1+1]=1
+               b[t2+1][r2]=2
+               for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==(t2+1) and (r1+1)==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1+=1
+            t2+=1
+#2          
+         if m1=='L' and m2=='R':
+            b[t1][r1]=4
+            p1=b[t1][r1-1]
+            b[t2][r2]=4
+            p2=b[t2][r2+1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1][r1-1]=1
+                b[t2][r2+1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==t2 and (r1-1)==(r2+1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1-=1
+            r2+=1
+         if m1=='L' and m2=='L':
+            b[t1][r1]=4
+            p1=b[t1][r1-1]
+            b[t2][r2]=4
+            p2=b[t2][r2-1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1][r1-1]=1
+                b[t2][r2-1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==t2 and (r1-1)==(r2-1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1-=1
+            r2-=1
+         if m1=='L' and m2=='U':
+            b[t1][r1]=4
+            p1=b[t1][r1-1]
+            b[t2][r2]=4
+            p2=b[t2-1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1][r1-1]=1
+                b[t2-1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==(t2-1) and (r1-1)==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1-=1
+            t2-=1
+         if m1=='L' and m2=='D':
+            b[t1][r1]=4
+            p1=b[t1][r1-1]
+            b[t2][r2]=4
+            p2=b[t2+1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1][r1-1]=1
+                b[t2+1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if t1==(t2+1) and (r1-1)==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            r1-=1
+            t2+=1
+#3
+         if m1=='U' and m2=='R':
+            b[t1][r1]=4
+            p1=b[t1-1][r1]
+            b[t2][r2]=4
+            p2=b[t2][r2+1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1-1][r1]=1
+                b[t2][r2+1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1-1)==t2 and r1==(r2+1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1-=1
+            r2+=1
+         if m1=='U' and m2=='L':
+            b[t1][r1]=4
+            p1=b[t1-1][r1]
+            b[t2][r2]=4
+            p2=b[t2][r2-1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1-1][r1]=1
+                b[t2][r2-1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1-1)==t2 and r1==(r2-1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1-=1
+            r2-=1
+         if m1=='U' and m2=='U':
+            b[t1][r1]=4
+            p1=b[t1-1][r1]
+            b[t2][r2]=4
+            p2=b[t2-1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1-1][r1]=1
+                b[t2-1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1-1)==(t2-1) and r1==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1-=1
+            t2-=1
+         if m1=='U' and m2=='D':
+            b[t1][r1]=4
+            p1=b[t1-1][r1]
+            b[t2][r2]=4
+            p2=b[t2+1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1-1][r1]=1
+                b[t2+1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1-1)==(t2+1) and r1==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1-=1
+            t2+=1
+#4
+         if m1=='D' and m2=='R':
+            b[t1][r1]=4
+            p1=b[t1+1][r1]
+            b[t2][r2]=4
+            p2=b[t2][r2+1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1+1][r1]=1
+                b[t2][r2+1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1+1)==t2 and r1==(r2+1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1+=1
+            r2+=1
+         if m1=='D' and m2=='L':
+            b[t1][r1]=4
+            p1=b[t1+1][r1]
+            b[t2][r2]=4
+            p2=b[t2][r2-1]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1+1][r1]=1
+                b[t2][r2-1]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1+1)==t2 and r1==(r2-1):
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1+=1
+            r2-=1
+         if m1=='D' and m2=='U':
+            b[t1][r1]=4
+            p1=b[t1+1][r1]
+            b[t2][r2]=4
+            p2=b[t2-1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1+1][r1]=1
+                b[t2-1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1+1)==(t2-1) and r1==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1+=1
+            t2-=1
+         if m1=='D' and m2=='D':
+            b[t1][r1]=4
+            p1=b[t1+1][r1]
+            b[t2][r2]=4
+            p2=b[t2+1][r2]
+            if p1!=8 and p1!=4 and p2!=8 and p2!=4 and p1!=2 and p2!=1 :
+                b[t1+1][r1]=1
+                b[t2+1][r2]=2
+                for v in range(m):
+                    print(b[v])
+            if p1==8 or p1==4:
+                x=0
+                print('The game is over!Player2 is the winner!')
+                break
+            if p2==8 or p2==4:
+                x=0
+                print('The game is over!Player1 is the winner!')
+                break
+            if (t1+1)==(t2+1) and r1==r2:
+                x=0
+                print("Game ends in a tie!")
+                break
+            t1+=1
+            t2+=1
+else:
+    print('please enter Y or N and restart the game!')
+
+                
+
+
+    
