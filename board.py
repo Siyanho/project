a5ef01b2d7a75fcd546e396560c8790f97965b2f
Siyanho <95035715+Siyanho@users.noreply.github.com>
2021-12-11 11:47:16 +0000

    Update board.py

--- board.py
+++ board.py
@@ -14,20 +14,27 @@ class Board:
         print('=======How to manage it?==========')
         print('The two players are moving in turns on a grid board that is surrounded by walls.  ')
         print('A player can move in four directions (up, down, left, right). ')
-        print('Enter uppercase letters U,D,L, and R.')
+        print('NOTE:Enter uppercase letters U,D,L, and R.')
         print('As a player moves, it leaves a trail on the spaces it has traversed, which acts as a wall.')
         print('The game ends if at least one player crashes into the wall, another player or previously visited cell. ')
         print('The player who survives the longest win.  ')
         
     def bd(self):
-        a1=np.zeros((self.size,self.size))
-        a1[0]=8
+        a1=[]
+        for i in range(self.size):
+            a1.append([])
+            for j in range(self.size):
+                    a1[i].append(0)  
         a1[1][1]=1
         a1[self.size-2][self.size-2]=2
-        for i in range(self.size):
-            a1[i][0]=8
-            a1[i][self.size-1]=8
-        a1[self.size-1]=8
+        for k in range(self.size):
+            a1[k][0]=8
+            a1[k][self.size-1]=8
+        for f in range(self.size):
+            a1[0][f]=8
+            a1[self.size-1][f]=8
+        for g in range(self.size):
+            print(a1[g])
         return a1
         
     def pi(self):
