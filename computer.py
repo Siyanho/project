dc6970ede7fe8200027293bf2d1f175fe847ddbc
Siyanho <95035715+Siyanho@users.noreply.github.com>
2021-12-27 17:34:52 +0000

    Update computer.py

--- computer.py
+++ computer.py
@@ -5,13 +5,22 @@ Created on Sat Dec 11 14:07:53 2021
 @author: admin
 """
 from player import Player
+import random
 class Computer(Player):
     def __init__(self,size,PlayerNumber,PlayerID,positionRow,positionCol):
-        super().__init__(size,PlayerNumber)
-        self.PlayerID=PlayerID
-        self.positionRow=positionRow
-        self.positionRow=positionCol
-        b=Board.bd()
+        super().__init__(size,PlayerNumber,PlayerID,positionRow,positionCol)
+        
+        
+    def mov(self):
+        move_list=['R','L','U','D']
+        moved=random.choice(move_list)
+        return moved
+
+# =============================================================================
+# computer1=Computer(5,2,2,4,4)       
+# m2=computer1.mov()
+# print(m2)
+# =============================================================================
         
             
         
