1c01269c0fff60f8c96c22aff4a9605e695d601b
Siyanho <95035715+Siyanho@users.noreply.github.com>
2022-01-07 23:24:31 +0000

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,26 +1,28 @@
-# -*- coding: utf-8 -*-
+#Part1 Siyan Huo bm21725@bristol.ac.uk
 """
 Created on Sat Dec 11 14:07:53 2021
 
-@author: admin
+define the Computer class
+a subclass of the Player class
+
+@author: Siyan Huo
 """
 from player import Player
 import random
 class Computer(Player):
     def __init__(self,PlayerID,positionRow,positionCol):
+        #Inherit player properties
         super().__init__(PlayerID,positionRow,positionCol)
         
-        
+    #Define the movement of the computer   
     def mov(self):
         move_list=['R','L','U','D']
+        #Select one at random from the list
         moved=random.choice(move_list)
+        #retunr the movement of the computer
         return moved
 
-# =============================================================================
-# computer1=Computer(5,2,2,4,4)       
-# m2=computer1.mov()
-# print(m2)
-# =============================================================================
+
         
             
         
