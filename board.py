9494c0808f610eae7c547c9cb8b0d0dcf24e78e0
Siyanho <95035715+Siyanho@users.noreply.github.com>
2022-01-07 22:46:24 +0000

    Update board.py

--- board.py
+++ board.py
@@ -1,9 +1,17 @@
+#Part1 Siyan Huo bm21725@bristol.ac.uk
+
+'''
+define the board class
+'''
+
 #CLASS1
 class Board:
+    #Defining basic properties
     def __init__(self,size,PlayerNumber):
         self.size=size
         self.PlayerNumber=PlayerNumber
     
+    #Game instructions
     def md(self):
         print('Hello! Welcome!')
         print('The number 8 represents the wall,')
@@ -17,24 +25,32 @@ class Board:
         print('As a player moves, it leaves a trail on the spaces it has traversed, which acts as a wall.')
         print('The game ends if at least one player crashes into the wall, another player or previously visited cell. ')
         print('The player who survives the longest win.  ')
-        
+    
+    #the default board    
     def bd(self):
+        #Define an empty list
         a1=[]
+        # Generates nested lists in empty lists
         for i in range(self.size):
             a1.append([])
             for j in range(self.size):
                     a1[i].append(0)  
+        #Player 1 is represented by the number 1
         a1[1][1]=1
+        #Player 2 is represented by the number 2
         a1[self.size-2][self.size-2]=2
+        #Build a wall, representing the wall with the number 8
         for k in range(self.size):
             a1[k][0]=8
             a1[k][self.size-1]=8
         for f in range(self.size):
             a1[0][f]=8
             a1[self.size-1][f]=8
+        #For aesthetics, traverse the list to print
         for g in range(self.size):
             print(a1[g])
         return a1
-        
+    
+    #show there are how many players
     def pi(self):
         print('There are '+str(self.PlayerNumber)+' players totally.')
